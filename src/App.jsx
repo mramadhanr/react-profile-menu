792bbbb7b83ed48ca62de6345f0f5614bd26ecba
Ramadhan4 <muhamadramadhanrahmat@gmail.com>
2019-09-08 17:52:58 +0700

    Integrating with eslint + prettier and adding airbnb rule

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ProfileMenu from './components/ProfileMenu.js/ProfileMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProfileMenu.js/ProfileMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileMenu = () => {
+  return (
+    <div>
+      <p>Profile Menu</p>
+    </div>
+  );
+};
+
+export default ProfileMenu;
